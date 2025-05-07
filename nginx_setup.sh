ab91b8f27bddf3fb659c7f4f1a0ddec8c58ca422
Bo Shang <bostealsfromtrials@gmail.com>
2025-05-07 18:14:32 -0400

    Create nginx_setup.sh

---
+++ nginx_setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+sudo apt update
+sudo apt install -y nginx
+
+sudo ln -s /etc/nginx/sites-available/WHATEVER_THE_FUCK /etc/nginx/sites-enabled/
+
+sudo nginx -t
+sudo systemctl reload nginx
+sudo systemctl restart nginx
